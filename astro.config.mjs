551ae1eee23879b68415efaff972898f915f0ca0
Kevin Alejandro <lealtorreskevin@gmail.com>
2024-05-29 19:37:21 -0600

    arreglando problemas de deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://amdi.github.io',
+    site: 'https://selectivo-amdi.github.io/amdi.github.io/',
 });
