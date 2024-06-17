ca2db4f4c23595df0f71fc926e755aaa42af45fd
Kevin Alejandro <lealtorreskevin@gmail.com>
2024-06-17 14:55:35 -0600

    Avances del día

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig, squooshImageService } from 'astro/config';
 import db from "@astrojs/db";
 import webVitals from "@astrojs/web-vitals";
+import node from "@astrojs/node";
 export default /** @type {import('astro').AstroUserConfig} */defineConfig({
   site: 'https://selectivo-amdi.github.io/amdi.github.io/',
   image: {
     service: squooshImageService()
   },
-  integrations: [db(), webVitals()]
+  integrations: [db(), webVitals()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
