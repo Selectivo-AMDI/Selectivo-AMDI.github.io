47d6ce04aba5e9572129521e5e7d2c0301232a7f
Kevin Alejandro <lealtorreskevin@gmail.com>
2024-06-17 16:10:21 -0600

    Se eliminó el componente de DB y de SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig, squooshImageService } from 'astro/config';
-import db from "@astrojs/db";
-import webVitals from "@astrojs/web-vitals";
-import node from "@astrojs/node";
+
 export default /** @type {import('astro').AstroUserConfig} */defineConfig({
   site: 'https://selectivo-amdi.github.io/amdi.github.io/',
   image: {
--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,11 @@
+/*
+
 import { db } from 'astro:db';
 
 // https://astro.build/db/seed
 export default async function seed() {
 	// TODO
 }
+
+
+*/
