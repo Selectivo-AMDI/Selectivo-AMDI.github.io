dcfd240961305292d7715c1800173d9d5b4dcce0
Kevin Alejandro <lealtorreskevin@gmail.com>
2024-06-17 13:35:09 -0600

    1) Integración con AstroDB;

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig, squooshImageService } from 'astro/config';
-
-// https://astro.build/config
-export default /** @type {import('astro').AstroUserConfig} */ defineConfig({
-    site: 'https://selectivo-amdi.github.io/amdi.github.io/',
-    image: {
-        service: squooshImageService(),
-      },
+import db from "@astrojs/db";
+import webVitals from "@astrojs/web-vitals";
+export default /** @type {import('astro').AstroUserConfig} */defineConfig({
+  site: 'https://selectivo-amdi.github.io/amdi.github.io/',
+  image: {
+    service: squooshImageService()
+  },
+  integrations: [db(), webVitals()]
 });
---
+++ db/config.ts
@@ -0,0 +1,24 @@
+// https://astro.build/db/config
+import { defineDb, defineTable, column } from 'astro:db';
+
+const Equipo = defineTable({
+  columns: {
+    nombre: column.text(),
+    goles: column.number(),
+    visitante: column.boolean(),
+    imagen: column.text(),
+  }
+})
+
+const Partido = defineTable({
+  columns: {
+    visitante: column.text(),
+    local: column.text(),
+    fecha: column.date(),
+    lugar: column.text(),
+  }
+})
+
+export default defineDb({
+  tables: { Equipo, Partido },
+})
