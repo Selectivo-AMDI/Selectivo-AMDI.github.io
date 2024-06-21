082b90fc26fbb07febbeed4e4c1b28b5a272b17b
Kevin Alejandro <lealtorreskevin@gmail.com>
2024-06-20 18:17:51 -0600

    1)Características de SEO integradas; 2)sitemap integrado

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig, squooshImageService } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default /** @type {import('astro').AstroUserConfig} */defineConfig({
   site: 'https://selectivo-amdi.github.io/amdi.github.io/',
   image: {
     service: squooshImageService()
   },
+  integrations: [
+    sitemap(),
+  ]
 });
