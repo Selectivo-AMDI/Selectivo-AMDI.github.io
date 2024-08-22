4e071a228289525fdc5ac9396b6b5be31385d439
Kevin Alejandro Leal Torres <lealtorreskevin@gmail.com>
2024-08-21 19:34:35 -0600

    intento 6 por optimizar imgs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@ import { defineConfig, squooshImageService } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 /*import vercel from "@astrojs/vercel/serverless";*/
 import react from "@astrojs/react";
+import compressor from "astro-compressor";
 export default /** @type {import('astro').AstroUserConfig} */defineConfig({
   site: 'https://selectivo-amdi.github.io/amdi.github.io/',
   image: {
     service: squooshImageService()
   },
-  integrations: [sitemap(), react()],
-  prefetch: true,
-/*  output: "server",*/
-/*  adapter: vercel() */
+  integrations: [sitemap(), react(), compressor({ gzip: false, brotli: true })],
+  prefetch: true
+  /*  output: "server",*/
+  /*  adapter: vercel() */
 });
