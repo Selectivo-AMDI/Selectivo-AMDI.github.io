e5592d3f10924eeb2d5c72104e0cebc3f955b8d0
Kevin Alejandro Leal Torres <lealtorreskevin@gmail.com>
2024-10-17 12:55:17 -0600

    Se arreglaron errores con los types storybloks

---
+++ src/images/iconos/README.md
@@ -0,0 +1,2 @@
+Páginas de donde recuperé alguno de los iconos:
+https://www.svgrepo.com/collection/solar-bold-icons/10
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,19 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: 'selector',
-  content: [
-    './src/**/*.{astro,html,js,jsx,ts,tsx}',
-    '!./src/pages/noticias/**/*.{astro,html,js,jsx,ts,tsx}',
-  ],
-  theme: {
-    extend: {
-      colors: {
-        'amdi-green': '#00A000',
-        'amdi-olive': '#787703',
-        'amdi-white': '#FEFEFE',
-        'amdi-black': '#000000',
-        'amdi-maroon': '#853504',
-      }
-    },
+export const darkMode = 'selector';
+export const content = [
+  './src/**/*.{astro,html,js,jsx,ts,tsx}',
+  '!./src/pages/noticias/**/*.{astro,html,js,jsx,ts,tsx}',
+];
+export const theme = {
+  extend: {
+    colors: {
+      'amdi-green': '#00A000',
+      'amdi-olive': '#787703',
+      'amdi-white': '#FEFEFE',
+      'amdi-black': '#000000',
+      'amdi-maroon': '#853504',
+    }
   },
-  plugins: [],
-}
+};
+export const plugins = [];
 
